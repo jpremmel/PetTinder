2ebbcca2b9ee6cbd80ae47c7c6a2b9e4b8045a48
Anita Kemp and Jacqueline Remmel <jacqueline@>
2019-11-04 13:17:30 -0800

    Add Identity

--- PetTinderMVC/Controllers/PetsController.cs
+++ PetTinderMVC/Controllers/PetsController.cs
@@ -1,11 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using PetTinderMVC.Models;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace PetTinderMVC.Controllers
 {
+    [Authorize]
     public class PetsController : Controller
     {
+        private readonly PetTinderMVCContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PetsController(UserManager<ApplicationUser> userManager, PetTinderMVCContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             var allPets = Pet.GetPets();
